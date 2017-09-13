107053ddd9b1f947dc8757a2a108592021793f5e
umar-qureshi2 <umar.mcs@gmail.com>
2017-09-13 17:31:09 +0400

    added use of dapper with tvp

---
+++ HelloDapper/HelloDapper/Extensions.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDapper
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// This extension converts an enumerable set to a Dapper TVP
+        /// </summary>
+        /// <typeparam name="T">type of enumerbale</typeparam>
+        /// <param name="enumerable">list of values</param>
+        /// <param name="typeName">database type name</param>
+        /// <param name="orderedColumnNames">if more than one column in a TVP, 
+        /// columns order must mtach order of columns in TVP</param>
+        /// <returns>a custom query parameter</returns>
+        public static SqlMapper.ICustomQueryParameter AsTableValuedParameter<T>
+            (this IEnumerable<T> enumerable,
+            string typeName, IEnumerable<string> orderedColumnNames = null)
+        {
+            var dataTable = new DataTable();
+            if (typeof(T).IsValueType || typeof(T).FullName.Equals("System.String"))
+            {
+                dataTable.Columns.Add(orderedColumnNames == null ?
+                    "NONAME" : orderedColumnNames.First(), typeof(T));
+                foreach (T obj in enumerable)
+                {
+                    dataTable.Rows.Add(obj);
+                }
+            }
+            else
+            {
+                PropertyInfo[] properties = typeof(T).GetProperties
+                    (BindingFlags.Public | BindingFlags.Instance);
+                PropertyInfo[] readableProperties = properties.Where
+                    (w => w.CanRead).ToArray();
+                if (readableProperties.Length > 1 && orderedColumnNames == null)
+                    throw new ArgumentException("Ordered list of column names must be provided when TVP contains more than one column");
+
+
+                var columnNames = (orderedColumnNames ??
+                    readableProperties.Select(s => s.Name)).ToArray();
+                foreach (string name in columnNames)
+                {
+                    dataTable.Columns.Add(name, readableProperties.Single
+                        (s => s.Name.Equals(name)).PropertyType);
+                }
+
+                foreach (T obj in enumerable)
+                {
+                    dataTable.Rows.Add(
+                        columnNames.Select(s => readableProperties.Single
+                            (s2 => s2.Name.Equals(s)).GetValue(obj))
+                            .ToArray());
+                }
+            }
+            return dataTable.AsTableValuedParameter(typeName);
+        }
+    }
+}
--- HelloDapper/HelloDapper/Program.cs
+++ HelloDapper/HelloDapper/Program.cs
@@ -8,6 +8,7 @@ using System.Data.SqlClient;
 using System.Collections;
 using Dapper.Contrib.Extensions;
 using Dapper.Contrib;
+using System.Data;
 
 namespace HelloDapper
 {
@@ -18,7 +19,12 @@ namespace HelloDapper
             using (var conn = new SqlConnection("Data Source=LAPTOP-6Q7L361S\\MSSQLDEV;Initial Catalog=Northwind;User ID=sa;Password=Sa@123456"))
             {
                 conn.Open();
+                var existingtopsuppliers = conn
+                    .Query<Supplier>("select top 2 CompanyName, City from Suppliers");
+                DataTable dt = new DataTable();
 
+                var parameters = existingtopsuppliers.AsTableValuedParameter("SupplierBasic", new string[] { "CompanyName", "City" });
+                var spResult = conn.Execute("ImportBasicSuppliers",new { suppliers = parameters }, commandType: CommandType.StoredProcedure);
             }
             Console.WriteLine("Please enter any key to exit");
             Console.ReadKey();
