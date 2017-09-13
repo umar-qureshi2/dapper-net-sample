3afe60d0c9903a4a38e77c778b4665a45b242005
umar-qureshi2 <umar.mcs@gmail.com>
2017-09-13 17:54:58 +0400

    dapper with tvp

--- HelloDapper/HelloDapper/DapperWithTVP.cs
+++ HelloDapper/HelloDapper/DapperWithTVP.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,20 @@ namespace HelloDapper
 {
     class DapperWithTVP
     {
+        public void InsertMultipleRowsWODT()
+        {
+            using (var conn = new SqlConnection("Data Source=LAPTOP-6Q7L361S\\MSSQLDEV;Initial Catalog=Northwind;User ID=sa;Password=Sa@123456"))
+            {
+                conn.Open();
+                var existingtopsuppliers = conn
+                    .Query<Supplier>("select top 2 CompanyName, City from Suppliers");
+                DataTable dt = new DataTable();
 
+                var parameters = existingtopsuppliers.AsTableValuedParameter("SupplierBasic", new string[] { "CompanyName", "City" });
+                var spResult = conn.Execute("ImportBasicSuppliers", new { suppliers = parameters }, commandType: CommandType.StoredProcedure);
+            }
+            Console.WriteLine("Please enter any key to exit");
+            Console.ReadKey();
+        }
     }
 }
