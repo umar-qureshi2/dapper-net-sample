536214009bd382d23a0ba7f7faf4c05a6bf6701c
umar-qureshi2 <umar.mcs@gmail.com>
2017-09-14 12:07:57 +0400

    sql builder basic added

---
+++ HelloDapper/HelloDapper/DapperSqlBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDapper
+{
+    public class DapperSqlBuilder
+    {
+        public void SelectById()
+        {
+            using (var conn = new SqlConnection("Data Source=LAPTOP-6Q7L361S\\MSSQLDEV;Initial Catalog=Northwind;User ID=sa;Password=Sa@123456"))
+            {
+                conn.Open();
+                SqlBuilder builder = new SqlBuilder();
+                var template = builder.AddTemplate("SELECT /**select**/ from Suppliers /**where**/");
+                builder.Select("*");
+                builder.Where("SupplierID = @SupplierID", new { SupplierID = 2 });
+                var sql = template.RawSql;
+                var supplier = conn.Query<Supplier>(sql, template.Parameters);
+                ObjectDumper.Write(supplier);
+            }
+            Console.WriteLine("Please enter any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
--- HelloDapper/HelloDapper/Program.cs
+++ HelloDapper/HelloDapper/Program.cs
@@ -19,12 +19,13 @@ namespace HelloDapper
             using (var conn = new SqlConnection("Data Source=LAPTOP-6Q7L361S\\MSSQLDEV;Initial Catalog=Northwind;User ID=sa;Password=Sa@123456"))
             {
                 conn.Open();
-                var existingtopsuppliers = conn
-                    .Query<Supplier>("select top 2 CompanyName, City from Suppliers");
-                DataTable dt = new DataTable();
-
-                var parameters = existingtopsuppliers.AsTableValuedParameter("SupplierBasic", new string[] { "CompanyName", "City" });
-                var spResult = conn.Execute("ImportBasicSuppliers",new { suppliers = parameters }, commandType: CommandType.StoredProcedure);
+                SqlBuilder builder = new SqlBuilder();
+                var template = builder.AddTemplate("SELECT /**select**/ from Suppliers /**where**/");
+                builder.Select("*");
+                builder.Where("SupplierID = @SupplierID", new {SupplierID = 2 });
+                var sql = template.RawSql;
+                var supplier = conn.Query<Supplier>(sql, template.Parameters);
+                ObjectDumper.Write(supplier);
             }
             Console.WriteLine("Please enter any key to exit");
             Console.ReadKey();
